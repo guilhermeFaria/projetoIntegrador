95e54bfd4dd8dc9d7121f0e2e278279b6f01d12f
Denis Campos <deniscpaula@hotmail.com>
2017-06-09 13:21:04 -0300

    Correção da rota

--- web-service/src/main/java/br/gov/sp/fatec/web/controller/PresencaController.java
+++ web-service/src/main/java/br/gov/sp/fatec/web/controller/PresencaController.java
@@ -47,7 +47,7 @@ public class PresencaController {
 	
 	}
 	@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_PROF')")
-	@RequestMapping(value = "/list/{disciplina}/{data}")
+	@RequestMapping(value = "/get/{disciplina}/{data}")
 	public ResponseEntity<Collection<Presenca>> getAll(@PathVariable(value="disciplina") Long disciplina, @PathVariable(value="data") Long dataMS){
 		Date data = new Date(dataMS);
 		return new ResponseEntity<Collection<Presenca>>(presencaService.buscarTodos(disciplina, data),HttpStatus.OK);
